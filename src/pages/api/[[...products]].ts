c1cea66bff6be838cc623bd0b9ee530eae1c4b60
4dydd <haidiraditya16@gmail.com>
2025-04-07 16:06:11 +0800

    next-viv-code-studio -> (19 bonus) optimasi dikit

--- src/pages/api/[[...products]].ts
+++ src/pages/api/[[...products]].ts
@@ -31,27 +31,46 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | DataError>
 ) {
+  //
+  //
+  //
+  //
+  //
+  // ========================================================================================
+  // Periksa apakah permintaan memiliki parameter kueri untuk mengambil produk berdasarkan ID
+  // ========================================================================================
   if (req.query.products?.[1]) {
     const data = await retrieveDataById("products", req.query.products[1]);
 
     if (data) {
       res.status(200).json({ status: true, statusCode: 200, data });
     } else {
-      res.status(500).json({
+      res.status(404).json({
         status: false,
-        statusCode: 500,
-        data: ["Data tidak ditemukan!"],
+        statusCode: 404,
+        data: ["Halaman Tidak Ditemukan!"],
       });
     }
-  } else {
+  }
+  //
+  //
+  //
+  //
+  //
+  // =======================================================================================================
+  // Ambil semua produk jika permintaan tidak memiliki parameter kueri untuk mengambil produk berdasarkan ID
+  // =======================================================================================================
+  else {
     const data = await retrieveData("products");
 
     if (data) {
       res.status(200).json({ status: true, statusCode: 200, data });
     } else {
-      res
-        .status(400)
-        .json({ status: false, statusCode: 400, data: ["nggk ada datanya"] });
+      res.status(400).json({
+        status: false,
+        statusCode: 400,
+        data: ["Tidak ada data yang ditemukan!"],
+      });
     }
 
     // res.status(200).json({ status: true, statusCode: 200, data });
